63feb19c967334b8b3ec8daa832ca142c6aa92be
koding1 <koding1@naver.com>
2022-01-23 18:36:02 +0900

    status bar 해결

--- MainScreen.js
+++ MainScreen.js
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 import { theme } from "./colors";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import StackNavigator from "./MainScreens/StackNavigator";
@@ -13,25 +12,28 @@ const Tab = createBottomTabNavigator();
 function MainScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.test}>
+      
+      <StatusBar style="auto" />
+
       <View>
         <Text>top</Text>
       </View>
-        <Tab.Navigator>
-          <Tab.Screen name="홈" component={StackNavigator} />
-          <Tab.Screen name="신규" component={NewUserScreen} />
-          <Tab.Screen name="과팅 & 미팅" component={BlindDateScreen} />
-        </Tab.Navigator>
-        <View>
+      <Tab.Navigator>
+        <Tab.Screen name="홈" component={StackNavigator} />
+        <Tab.Screen name="신규" component={NewUserScreen} />
+        <Tab.Screen name="과팅 & 미팅" component={BlindDateScreen} />
+      </Tab.Navigator>
+      <View>
         <Text>bottom</Text>
       </View>
 
-      <StatusBar style="auto" />
+      
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  test : {
+  test: {
     flex: 1,
   },
 });
--- MainScreens/Temporary.js
+++ MainScreens/Temporary.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 const NewUserScreen = ({}) => {
-  console.log("디버깅 : NewUserScreen");
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>신규!</Text>
@@ -10,7 +9,6 @@ const NewUserScreen = ({}) => {
 };
 
 const BlindDateScreen = ({}) => {
-  console.log("디버깅 : BlindDateScreen");
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>과팅 & 미팅!</Text>

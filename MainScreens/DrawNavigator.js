7c78ad15e6187e8661b8e459c1f722103815d4bd
sseunghoon <sseunghun99@naver.com>
2022-01-28 13:36:08 +0900

    Add Side Bar Content:

--- MainScreens/DrawNavigator.js
+++ MainScreens/DrawNavigator.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
+import { StyleSheet, Text, View, Button, Image} from "react-native";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-} from "react-native";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomeScreen from './HomeScreen';
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import HomeScreen from "./HomeScreen";
 
 function MyInfoScreen({ navigation }) {
   return (
@@ -40,18 +40,47 @@ function AllianceScreen({ navigation }) {
   );
 }
 
-console.log("DrawNavigator Check:");
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="Help" onPress={() => alert("Link to help")} />
+    </DrawerContentScrollView>
+  );
+}
 
 const Drawer = createDrawerNavigator();
 const DrawNavigator = () => {
   return (
-    <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="홈" component={HomeScreen} />
-        <Drawer.Screen name="내 정보" component={MyInfoScreen} />
-        <Drawer.Screen name="공지사항" component={NotificationsScreen} />
-        <Drawer.Screen name="자주하는 질문" component={QnAScreen} />
-        <Drawer.Screen name="과팅 제휴" component={AllianceScreen} />
-      </Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "#c6cbef",
+          width: 240,
+        },
+        drawerIcon: ({ tintColor }) => (
+          <Image
+            source={require("../image/bg.jpg")}
+            style={[styles.icon, { tintColor: tintColor }]}
+          />
+        ),
+      }}
+      initialRouteName="Home"
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="홈" component={HomeScreen} />
+      <Drawer.Screen name="내 정보" component={MyInfoScreen} />
+      <Drawer.Screen name="공지사항" component={NotificationsScreen} />
+      <Drawer.Screen name="자주하는 질문" component={QnAScreen} />
+      <Drawer.Screen name="과팅 제휴" component={AllianceScreen} />
+    </Drawer.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
 export default DrawNavigator;
